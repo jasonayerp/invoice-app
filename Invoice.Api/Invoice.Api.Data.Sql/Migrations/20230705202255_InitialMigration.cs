bd66a63f540735da8673294f6119633cb5ca273d
TEMPEST\jason <jason.ayer.p@gmail.com>
2023-07-05 15:25:10 -0500

    Added migrations and migrations scripts

---
+++ Invoice.Api/Invoice.Api.Data.Sql/Migrations/20230705202255_InitialMigration.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoice.Api.Data.Sql.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                schema: "dbo",
+                columns: table => new
+                {
+                    AddressId = table.Column<long>(type: "BIGINT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false, defaultValueSql: "NEWID()"),
+                    AddressLine1 = table.Column<string>(type: "NVARCHAR(128)", nullable: false),
+                    AddressLine2 = table.Column<string>(type: "NVARCHAR(128)", nullable: true, defaultValueSql: "NULL"),
+                    AddressLine3 = table.Column<string>(type: "NVARCHAR(128)", nullable: true, defaultValueSql: "NULL"),
+                    AddressLine4 = table.Column<string>(type: "NVARCHAR(128)", nullable: true, defaultValueSql: "NULL"),
+                    City = table.Column<string>(type: "NVARCHAR(128)", nullable: false),
+                    Region = table.Column<string>(type: "NVARCHAR(128)", nullable: false),
+                    PostalCode = table.Column<string>(type: "NVARCHAR(128)", nullable: false),
+                    CountryCode = table.Column<string>(type: "NCHAR(2)", nullable: false),
+                    UtcCreatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: false),
+                    UtcUpdatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL"),
+                    UtcDeletedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                schema: "dbo",
+                columns: table => new
+                {
+                    ClientId = table.Column<long>(type: "BIGINT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "NVARCHAR(50)", nullable: false),
+                    Phone = table.Column<string>(type: "NVARCHAR(50)", nullable: true, defaultValueSql: "NULL"),
+                    Email = table.Column<string>(type: "NVARCHAR(50)", nullable: true, defaultValueSql: "NULL"),
+                    UtcCreatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: false),
+                    UtcUpdatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL"),
+                    UtcDeletedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                schema: "dbo",
+                columns: table => new
+                {
+                    InvoiceId = table.Column<long>(type: "BIGINT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false, defaultValueSql: "NEWID()"),
+                    Number = table.Column<string>(type: "NVARCHAR(30)", nullable: false),
+                    UtcDate = table.Column<DateTime>(type: "DATETIME2", nullable: false),
+                    Status = table.Column<short>(type: "SMALLINT", nullable: false),
+                    PaymentTerm = table.Column<short>(type: "SMALLINT", nullable: false),
+                    BillFromAddressId = table.Column<long>(type: "BIGINT", nullable: false),
+                    BillToAddressId = table.Column<long>(type: "BIGINT", nullable: false),
+                    ClientId = table.Column<long>(type: "BIGINT", nullable: false),
+                    UtcCreatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: false),
+                    UtcUpdatedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL"),
+                    UtcDeletedDate = table.Column<DateTime>(type: "DATETIME2", nullable: true, defaultValueSql: "NULL")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.InvoiceId)
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Addresses_BillFromAddressId",
+                        column: x => x.BillFromAddressId,
+                        principalSchema: "dbo",
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Addresses_BillToAddressId",
+                        column: x => x.BillToAddressId,
+                        principalSchema: "dbo",
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalSchema: "dbo",
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceItems",
+                schema: "dbo",
+                columns: table => new
+                {
+                    InvoiceItemId = table.Column<long>(type: "BIGINT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InvoiceId = table.Column<long>(type: "BIGINT", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR(128)", nullable: false),
+                    Quantity = table.Column<int>(type: "INT", nullable: false),
+                    Amount = table.Column<decimal>(type: "DECIMAL(19,4)", precision: 19, scale: 4, nullable: false),
+                    UtcCreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UtcUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UtcDeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceItem", x => x.InvoiceItemId)
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_InvoiceItem_Invoice_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalSchema: "dbo",
+                        principalTable: "Invoices",
+                        principalColumn: "InvoiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_City",
+                schema: "dbo",
+                table: "Addresses",
+                column: "City")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_CountryCode",
+                schema: "dbo",
+                table: "Addresses",
+                column: "CountryCode")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_Region",
+                schema: "dbo",
+                table: "Addresses",
+                column: "Region")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Addresses_Address",
+                schema: "dbo",
+                table: "Addresses",
+                columns: new[] { "AddressLine1", "City", "Region", "PostalCode", "CountryCode" },
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_Guid",
+                schema: "dbo",
+                table: "Clients",
+                column: "Guid")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Client_Name",
+                schema: "dbo",
+                table: "Clients",
+                column: "Name",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceItems_InvoiceId",
+                schema: "dbo",
+                table: "InvoiceItems",
+                column: "InvoiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_InvoiceItems_Description",
+                schema: "dbo",
+                table: "InvoiceItems",
+                column: "Description",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_BillFromAddressId",
+                schema: "dbo",
+                table: "Invoices",
+                column: "BillFromAddressId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_BillToAddressId",
+                schema: "dbo",
+                table: "Invoices",
+                column: "BillToAddressId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_ClientId",
+                schema: "dbo",
+                table: "Invoices",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Invoices_Number",
+                schema: "dbo",
+                table: "Invoices",
+                column: "Number",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InvoiceItems",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Invoices",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Addresses",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Clients",
+                schema: "dbo");
+        }
+    }
+}
---
+++ Invoice.Api/Invoice.Api.Data.Sql/Migrations/Scripts/Migrations.sql
@@ -0,0 +1,172 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE TABLE [dbo].[Addresses] (
+        [AddressId] BIGINT NOT NULL IDENTITY,
+        [Guid] UNIQUEIDENTIFIER NOT NULL DEFAULT (NEWID()),
+        [AddressLine1] NVARCHAR(128) NOT NULL,
+        [AddressLine2] NVARCHAR(128) NULL DEFAULT (NULL),
+        [AddressLine3] NVARCHAR(128) NULL DEFAULT (NULL),
+        [AddressLine4] NVARCHAR(128) NULL DEFAULT (NULL),
+        [City] NVARCHAR(128) NOT NULL,
+        [Region] NVARCHAR(128) NOT NULL,
+        [PostalCode] NVARCHAR(128) NOT NULL,
+        [CountryCode] NCHAR(2) NOT NULL,
+        [UtcCreatedDate] DATETIME2 NOT NULL,
+        [UtcUpdatedDate] DATETIME2 NULL DEFAULT (NULL),
+        [UtcDeletedDate] DATETIME2 NULL DEFAULT (NULL),
+        CONSTRAINT [PK_Addresses] PRIMARY KEY CLUSTERED ([AddressId])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE TABLE [dbo].[Clients] (
+        [ClientId] BIGINT NOT NULL IDENTITY,
+        [Guid] UNIQUEIDENTIFIER NOT NULL DEFAULT (NEWID()),
+        [Name] NVARCHAR(50) NOT NULL,
+        [Phone] NVARCHAR(50) NULL DEFAULT (NULL),
+        [Email] NVARCHAR(50) NULL DEFAULT (NULL),
+        [UtcCreatedDate] DATETIME2 NOT NULL,
+        [UtcUpdatedDate] DATETIME2 NULL DEFAULT (NULL),
+        [UtcDeletedDate] DATETIME2 NULL DEFAULT (NULL),
+        CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([ClientId])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE TABLE [dbo].[Invoices] (
+        [InvoiceId] BIGINT NOT NULL IDENTITY,
+        [Guid] UNIQUEIDENTIFIER NOT NULL DEFAULT (NEWID()),
+        [Number] NVARCHAR(30) NOT NULL,
+        [UtcDate] DATETIME2 NOT NULL,
+        [Status] SMALLINT NOT NULL,
+        [PaymentTerm] SMALLINT NOT NULL,
+        [BillFromAddressId] BIGINT NOT NULL,
+        [BillToAddressId] BIGINT NOT NULL,
+        [ClientId] BIGINT NOT NULL,
+        [UtcCreatedDate] DATETIME2 NOT NULL,
+        [UtcUpdatedDate] DATETIME2 NULL DEFAULT (NULL),
+        [UtcDeletedDate] DATETIME2 NULL DEFAULT (NULL),
+        CONSTRAINT [PK_Invoices] PRIMARY KEY CLUSTERED ([InvoiceId]),
+        CONSTRAINT [FK_Invoices_Addresses_BillFromAddressId] FOREIGN KEY ([BillFromAddressId]) REFERENCES [dbo].[Addresses] ([AddressId]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Invoices_Addresses_BillToAddressId] FOREIGN KEY ([BillToAddressId]) REFERENCES [dbo].[Addresses] ([AddressId]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Invoices_Clients_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Clients] ([ClientId]) ON DELETE NO ACTION
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE TABLE [dbo].[InvoiceItems] (
+        [InvoiceItemId] BIGINT NOT NULL IDENTITY,
+        [InvoiceId] BIGINT NOT NULL,
+        [Description] NVARCHAR(128) NOT NULL,
+        [Quantity] INT NOT NULL,
+        [Amount] DECIMAL(19,4) NOT NULL,
+        [UtcCreatedDate] datetime2 NOT NULL,
+        [UtcUpdatedDate] datetime2 NULL,
+        [UtcDeletedDate] datetime2 NULL,
+        CONSTRAINT [PK_InvoiceItem] PRIMARY KEY CLUSTERED ([InvoiceItemId]),
+        CONSTRAINT [FK_InvoiceItem_Invoice_InvoiceId] FOREIGN KEY ([InvoiceId]) REFERENCES [dbo].[Invoices] ([InvoiceId]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_Addresses_City] ON [dbo].[Addresses] ([City]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_Addresses_CountryCode] ON [dbo].[Addresses] ([CountryCode]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_Addresses_Region] ON [dbo].[Addresses] ([Region]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE NONCLUSTERED INDEX [UX_Addresses_Address] ON [dbo].[Addresses] ([AddressLine1], [City], [Region], [PostalCode], [CountryCode]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_Client_Guid] ON [dbo].[Clients] ([Guid]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE NONCLUSTERED INDEX [UX_Client_Name] ON [dbo].[Clients] ([Name]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE INDEX [IX_InvoiceItems_InvoiceId] ON [dbo].[InvoiceItems] ([InvoiceId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE NONCLUSTERED INDEX [UX_InvoiceItems_Description] ON [dbo].[InvoiceItems] ([Description]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE INDEX [IX_Invoices_BillFromAddressId] ON [dbo].[Invoices] ([BillFromAddressId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE INDEX [IX_Invoices_BillToAddressId] ON [dbo].[Invoices] ([BillToAddressId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE INDEX [IX_Invoices_ClientId] ON [dbo].[Invoices] ([ClientId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    CREATE UNIQUE NONCLUSTERED INDEX [UX_Invoices_Number] ON [dbo].[Invoices] ([Number]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230705202255_InitialMigration')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230705202255_InitialMigration', N'6.0.19');
+END;
+GO
+
+COMMIT;
+GO
+
